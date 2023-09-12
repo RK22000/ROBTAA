688be9d4644787ede95cdcdcc83d1eebe6a5a2bb
Rahul <rahukand@gmail.com>
2023-09-12 05:54:45 -0700

    Extracted title card into component and wrote a little about site.

---
+++ src/lib/TitleCard.svelte
@@ -0,0 +1,27 @@
+<script>
+	/**@type {string} Path of picture to display in the title card */
+	export let title_pic;
+</script>
+
+<div class="overflow-hidden">
+	<div
+		class="
+    relative
+    scale-110 hover:scale-100 transition duration-1000
+    "
+	>
+		<div
+			class="
+        bg-davys_gray-100/70
+        absolute inset-0
+        flex flex-col justify-center text-center
+        hover:bg-transparent hover:text-transparent transition duration-1000
+        "
+		>
+        <div class='scale-90'>
+			<slot><!-- optional fallback is only display picture--></slot>
+        </div>
+		</div>
+		<img src={title_pic} alt="" />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,43 @@
 <script>
 	import { base } from '$app/paths';
+	import TitleCard from '$lib/TitleCard.svelte';
 </script>
 
-<div class="overflow-hidden">
-<div class="relative hover:scale-110 transition duration-1000">
-	<div
-		class="
-        bg-davys_gray-100/70
-        absolute inset-0
-        flex flex-col justify-center text-center
-        hover:bg-transparent hover:text-transparent transition duration-1000
-        "
-	>
-		<h1 class="text-5xl">ROBTAA</h1>
-		<h2>
-			Welcome to Rahul's Online Blog That's (hopefully) Accssible Anywhere with a reasonable
-			internet connection.
-		</h2>
-	</div>
-	<img src="{base}/PaperDragons.jpg" alt="" />
-</div>
+<TitleCard title_pic="{base}/PaperDragons.jpg">
+	<h1 class="text-5xl">ROBTAA</h1>
+	<h2>
+		Welcome to Rahul's Online Blog That's (hopefully) Accssible Anywhere with a reasonable internet
+		connection.
+	</h2>
+</TitleCard>
 
-</div>
+<article class="prose prose-invert">
+	<p>
+		Every once in a while I’ll have the urge to write something that I want to share with someone
+		else or my future self. Normally I’d just pester my room mate with such random thoughts, or I’d
+		ignore them by convincing myself that I haven’t got a good place or audience to write such
+		thoughts for.
+	</p>
+	<p>Not any more!</p>
+	<p>
+		Earlier this year(2023) I spent 2 days figuring out <a
+			href="https://jekyllrb.com/docs/step-by-step/01-setup/">Jekyll</a
+		>
+		and made <a href="https://rk22000.github.io/">this blog site</a>. I was actually procastinating
+		on my urge to write about something while procastinating on 3 homework assignments. But due to
+		that procastination I finally dipped my toes into web development.
+	</p>
+	<p>
+		Overall I like having a site where I could make blog posts. However I always felt restricted in
+		what I could do, likely due to my limited compentence with html, css, and js. I was never really
+		intersted in webdevelopment. However feeling that restriction lit a few embers in me to figure
+		out at least enough web dev that I sould be able to concive and hypothesize a way to make any
+		website my mind might dream up of.
+	</p>
+	<p>
+		Those embers finally caught fire and I'm now figuring out and using <a
+			href="https://svelte.dev/">Svelte</a
+		>
+		and <a href="https://tailwindcss.com/">Tailwind</a> to make the blog site that you are on right now.
+	</p>
+</article>
